9b82b2abb8c8c6f285fcdbe513b67daae73b3599
tetriv0217 <131021398+tetriv0217@users.noreply.github.com>
2023-07-20 00:18:13 +0530

    Updated With placeholder repetition was added in  index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+//  By Tejas 
+const placeholderTexts = [
+  "GeeksforGeeks Courses.",
+  "Data Structures and Algorithms.",
+  "Complete Interview Preparation!!",
+  "Learn C++,Java,Python..."
+];
+const inputElement = document.getElementById("plc");
+const loopInterval = 1500;
+const letterInterval = 90;
+let currentTextIndex = 0;
+let loop = true;
+
+function typeAndDisappearText(index, currentCharIndex) {
+  if (!loop) return;
+
+  const currentText = placeholderTexts[index];
+
+  if (currentCharIndex >= currentText.length) {
+    setTimeout(() => {
+      inputElement.setAttribute("placeholder", "");
+      setTimeout(() => typeAndDisappearText((index + 1) % placeholderTexts.length, 0), loopInterval);
+    }, loopInterval);
+  } else {
+    const textToShow = currentText.substring(0, currentCharIndex + 1);
+    inputElement.setAttribute("placeholder", textToShow);
+
+    setTimeout(() => typeAndDisappearText(index, currentCharIndex + 1), letterInterval);
+  }
+}
+
+inputElement.addEventListener("focus", function () {
+  loop = false;
+  inputElement.setAttribute("placeholder", "");
+});
+
+inputElement.addEventListener("blur", function () {
+  loop = true;
+  typeAndDisappearText(currentTextIndex, 0);
+});
+
+typeAndDisappearText(currentTextIndex, 0);
